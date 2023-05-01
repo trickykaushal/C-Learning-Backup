72ebf078c509e1e6bb130733936f3ea2d4cd0c31
Kaushal <trickykaushal@gmail.com>
2023-05-01 20:34:26 +0530

    DifferedvsImmediate Added

---
+++ ExecuteDefferedVsImmediate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExecuteDefferedVsImmediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = Student.GetStudents();
+            var AllStudent = students.Where(x => x.Marks > 700).Select(x => x).Count();
+
+            students.Add(new Student { Name = "Vinod", Marks = 890 });
+            Console.WriteLine($"COunt is {AllStudent}");
+
+
+            //foreach(var k in AllStudent)
+            //{
+
+            //    Console.WriteLine($"{k.Name} and the marks si : {k.Marks}");
+            //}
+        }
+    }
+}
---
+++ ExecuteDefferedVsImmediate/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExecuteDefferedVsImmediate
+{
+    class Student
+    {
+        //public Student(int marks, string name)
+        //{
+        //    Marks = marks;
+        //    Name = name;
+        //}
+
+        public int Marks { get; set; }
+        public string Name { get; set; }
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student> {
+                 new Student { Marks = 500, Name = "Kaushal" } ,
+                 new Student { Marks = 600, Name = "Manoj" } ,
+                 new Student { Marks = 700, Name = "Patel" } ,
+                 new Student { Marks = 800, Name = "Kajal" } ,
+                 new Student { Marks = 900, Name = "Garima" }
+                };
+            return students;
+
+        }
+
+    }
+}
